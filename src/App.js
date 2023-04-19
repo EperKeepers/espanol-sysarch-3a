d8bd7ae5e7d3603797f0e91747c42b0791b4ae13
Jennifer Español <ackermanjennifer19@gmail.com>
2023-04-19 15:41:55 +0800

    Third Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Home';
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,13 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <Link to="/" className="nav-link">Home page<span className="sr-only">(current)</span></Link>
+            <Link to="/" className="nav-link">Home page<span className="sr-only"></span></Link>
           </li>
           <li className="nav-item">
             <Link to="/about" className="nav-link">About page</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">Contact page</Link>
           </li>
         </ul>
       </div>
