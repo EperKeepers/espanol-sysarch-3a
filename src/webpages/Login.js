144c3b6fa5d8881ec3108b77ed2e5a6dc81e9955
Jennifer Español <ackermanjennifer19@gmail.com>
2023-05-01 23:32:18 +0800

    10th commit

--- src/webpages/Login.js
+++ src/webpages/Login.js
@@ -1,42 +1,12 @@
 import React, { useState } from 'react';
 import './Login.css';
+import Form from '../components/Form';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // handle form submission
-  };
-
   return (
-    <div className="container">
-      <div className="row justify-content-center mt-5">
-        <div className="col-md-6">
-          <div className="card">
-            <div className="card-header">
-              Login
-            </div>
-            <div className="card-body">
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label>Email</label>
-                  <input type="email" className="form-control" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)} required />
-                </div>
-                <div className="form-group">
-                  <label>Password</label>
-                  <input type="password" className="form-control" placeholder="Enter password" value={password} onChange={(event) => setPassword(event.target.value)} required />
-                </div>
-                <div className="spacer">
-                  <label></label>      
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div style={{width: 600, margin: 'auto', marginTop: 40}}>
+      <h3>Login</h3>
+      <Form/>
     </div>
   );
 };
